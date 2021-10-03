feb473b67ba2c94f2627601d225817988662439a
Yuki Matsukura <matsubokkuri@gmail.com>
2021-10-03 13:44:04 +0900

    [#57] fix for rubocop

--- test.rb
+++ test.rb
@@ -1,7 +1,5 @@
-require "bundler/setup"
+require 'bundler/setup'
 Bundler.require
-
-
 
 open_api_file = 'tanomimaster.yml'
 
@@ -9,14 +7,10 @@ open_api_file = 'tanomimaster.yml'
 document = Openapi3Parser.load_file(open_api_file)
 
 unless document.valid?
-  pp document.errors 
+  pp document.errors
   raise 'Syntax error found'
 end
-
-
 
 # Check by OpenAPIParser
 # Just ensure the file is parsable by OpenAPIParser
 open_api = OpenAPIParser.parse(YAML.load_file(open_api_file))
-
-
